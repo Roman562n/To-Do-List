db9f0c5df5a5b962e1a197bb80f070e8ef88ac87
Roman562n <shpakroman562@gmail.com>
2025-03-11 22:13:15 +0300

    Add JS logic

--- main.js
+++ main.js
@@ -2,22 +2,43 @@ const createTaskInputElement = document.querySelector('[data-js-create-task-inpu
 const createTaskButtonElement = document.querySelector('[data-js-create-task-button]')
 const todoListElement = document.querySelector('[data-js-todo-list]')
 
+function removeTask(element) {
+  element.remove()
+}
+
+todoListElement.addEventListener('click', (event) => {
+  const todoItemElement = event.target.closest('[data-js-todo-item]')
+  const deleteButtonElement = event.target.closest('[data-js-task-delete-button]')
+
+  if (deleteButtonElement) {
+    removeTask(todoItemElement)
+  }
+  else if (todoItemElement) {
+    todoItemElement.querySelector('[data-js-task-checkbox]').checked = false
+    console.log(event.target)
+    todoItemElement.classList.toggle('checked')
+
+    if (todoItemElement.classList.contains('checked')) {
+      todoItemElement.querySelector('[data-js-task-checkbox]').checked = true
+    }
+    
+  }
+})
+
+const taskCheckboxElements = todoListElement.querySelectorAll('[data-js-task-checkbox]')
+
 function addTodoItem() {
   const taskMessage = createTaskInputElement.value
   if (taskMessage === '') {
     return
   }
-  const newTaskMarkup = 
-  `<li class="todo__item" data-js-task-item>
-    <div class="todo__task task shadow-block">
-      <label class="task__label">
-        <input class="task__checkbox custom-checkbox" type="checkbox">
-        <span class="task__content">${taskMessage}</span>
-      </label>
+  const newTaskMarkup = `
+    <li class="todo__item task shadow-block" data-js-todo-item>
+      <input class="task__checkbox custom-checkbox" type="checkbox" data-js-task-checkbox>
+      <span class="task__content">${taskMessage}</span>
       <button class="task__delete-button button" type="button" aria-label="Delete task" title="Delete task" data-js-task-delete-button></button>
-    </div>
-  </li>`
-  
+    </li>`
+
   todoListElement.insertAdjacentHTML("afterbegin", newTaskMarkup)
   createTaskInputElement.value = ''
 }
@@ -32,8 +53,6 @@ createTaskInputElement.addEventListener('keydown', (event) => {
 })
 
 
-const taskCheckboxElement = document.querySelector('[data-js-task-checkbox]')
 
-console.log(taskCheckboxElement.checked)
 
 
